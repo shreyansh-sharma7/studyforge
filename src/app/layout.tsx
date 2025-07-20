4507a0cfd6b634f5e9a6b1548cf471cc757f8999
mr_miner <shreyanshsharma0708@gmail.com>
2025-07-21 05:03:05 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LeftNav from "./components/leftnav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/lib/fiesystem/script.ts
+++
@@ -1,3 +0,0 @@
-const GetCurrentDir = () => {};
-
-export default { GetCurrentDir };
